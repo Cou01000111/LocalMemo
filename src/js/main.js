10cb7b8a39a5a49f3f1068b4039bb0ad68608d31
courange <courange.c@gmail.com>
2021-08-02 07:18:36 +0900

    for now

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,23 @@
 import $ from "jquery";
-import Vue from "vue";
-var files: string | null = localStorage.getItem("localMemo");
+var files = localStorage.getItem("localMemo");
 if (files != null) {
-    var fileList:Array<string> = files.split(',');
-    fileList.forEach(file => {
+    var fileList = files.split(',');
+    fileList.forEach(function (file) {
         setFile(file);
     });
-}else{
+}
+else {
     setFile('untitled');
 }
-
 //tabにファイル名を追加
 //追加したモノにis-activeを追加
 //それ以外のタブからis-activeを削除
 //右のFILESに追加する
-function setFile(fileName:string) {
-
+function setFile(fileName) {
 }
-
-function addFileToTab(fileName:string) {
+function addFileToTab(fileName) {
     $('editorTabs').children('ul').append(getTabsItem(fileName));
 }
-
-function getTabsItem(fileName:string) {
+function getTabsItem(fileName) {
     return ('<li id="add"><a>' + fileName + ' </a></li>');
 }
